cc22ee26e5c42dc4eb4034fdb6d5db45fc5093d2
Eric Yen <eric@ericyen.com>
2023-08-27 06:35:44 +0000

    fix typo in dns

--- modules/headscale.nix
+++ modules/headscale.nix
@@ -33,12 +33,17 @@ in
       # tls_letsencrypt_listen = ":http = port 80";
       tls_letsencrypt_hostname = "${domain}";
       logtail.enabled = false;
-      #      ip_prefixes = [ "fd7a:115c:a1e0::/48" "100.64.0.0/10" ];
       ip_prefixes = [ "100.64.0.0/10" ];
       private_key_path = "/var/lib/headscale/private.key";
       noise.private_key_path = "/var/lib/headscale/noise_private.key";
+      disable_check_updates = true; # use nix to manage that
+      derp = {
+        server = {
+          enabled = false;
+        };
+      };
       dns_config = {
-        nameservers = [ "10.64.0.9" "1.1.1.1" ];
+        nameservers = [ "100.64.0.9" "100.64.0.16" ];
         baseDomain = "ts.lan";
         override_local_dns = true;
         extra_records = tailscale_dns_entries;
