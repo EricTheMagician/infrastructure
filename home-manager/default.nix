2e63f99805eed148c9c80ee2804a9b75f1d06218
Eric Yen <eric@ericyen.com>
2023-08-27 19:24:22 -0700

    remove tab from switching in coc

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -157,10 +157,10 @@ in {
       " no select by `"suggest.noselect": true` in your configuration file
       " NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
       " other plugin before putting this into your config
-      inoremap <silent><expr> <TAB>
-            \ coc#pum#visible() ? coc#pum#next(1) :
-            \ CheckBackspace() ? "\<Tab>" :
-            \ coc#refresh()
+      " inoremap <silent><expr> <TAB>
+      "      \ coc#pum#visible() ? coc#pum#next(1) :
+      "      \ CheckBackspace() ? "\<Tab>" :
+      "      \ coc#refresh()
       inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
 
       " Make <CR> to accept selected completion item or notify coc.nvim to format
