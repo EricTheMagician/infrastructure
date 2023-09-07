70b52c64df455f2de609188081418b3f8ce6305b
Eric Yen <eric@ericyen.com>
2023-09-07 01:01:43 -0700

    update to hercules

--- services/hercules-ci-agent.nix
+++ services/hercules-ci-agent.nix
@@ -8,7 +8,25 @@
   disabledModules = ["services/continuous-integration/hercules-ci-agent/common.nix"];
   imports = [(inputs.nixpkgs-unstable + "/nixos/modules/services/continuous-integration/hercules-ci-agent/common.nix")];
 
+  sops = {
+    # This is the actual specification of the secrets.
+    secrets."binary-caches.json" = {
+      mode = "0400";
+      sopsFile = ../secrets/nix-serve.yaml;
+      restartUnits = ["nix-serve.service" "hercules-ci-agent.service"];
+    };
+    secrets."cluster-join-token.key" = {
+      mode = "0400";
+      sopsFile = ../secrets/hercules.yaml;
+      restartUnits = ["hercules-ci-agent.service"];
+    };
+  };
+
   services.hercules-ci-agent = {
     enable = true;
+    settings = {
+      binaryCachesPath = config.sops.secrets."binary-caches.json".path;
+      clusterJoinTokenPath = config.sops.secrets."cluster-join-token.key".path;
+    };
   };
 }
