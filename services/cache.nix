2ff8c88d3c52d771a9b1bd0a0268fdbb46478e39
Eric Yen <eric@ericyen.com>
2023-09-07 00:40:04 -0700

    update cache

--- services/cache.nix
+++ services/cache.nix
@@ -8,23 +8,21 @@
 in {
   sops = {
     # This is the actual specification of the secrets.
-    secrets.nix-serve.private = {
+    secrets."nix-serve.private" = {
       mode = "0400";
       sopsFile = ../secrets/nix-serve.yaml;
-      group = "nix-serve";
-      owner = "nix-serve";
       restartUnits = ["nix-serve.service"];
     };
   };
 
   services.nix-serve = {
     enable = true;
-    secretKeyFile = config.sops.secrets.nix-serve.private.path;
-    #package = unstable.nix-serve-ng;
+    secretKeyFile = config.sops.secrets."nix-serve.private".path;
   };
 
   nginx.virtualHosts.${nix-cache-domain} = {
     useACMEHost = "eyen.ca";
+    forceSSL = true;
     locations."/" = {
       proxyPass = "http://${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}";
     };
