a6c14fe39d536f84e73e23713c7704f0951bf83f
Eric Yen <eric@ericyen.com>
2023-09-16 07:30:35 -0700

    add maximum number of parallel download jobs to workstation

--- systems/nixos-workstation-configuration.nix
+++ systems/nixos-workstation-configuration.nix
@@ -42,6 +42,9 @@ in {
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
+
+      # download in parallel from nix-cache
+      max-substitution-jobs = 32;
     };
   };
 
