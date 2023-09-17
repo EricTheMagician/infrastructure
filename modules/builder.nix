457c301155e372edc66873e205511ea75d7a4839
Eric Yen <eric@ericyen.com>
2023-09-16 17:07:24 -0700

    add new script for simpler building and caching

--- modules/builder.nix
+++ modules/builder.nix
@@ -7,16 +7,19 @@
 }: let
   nix-server-secret = config.sops.secrets."nix-serve.private".path;
   upload-cache-script =
-    pkgs.writeShellScript "upload-cache-script.sh"
+    pkgs.writeShellScriptBin "build-and-upload-to-cache.sh"
     ''
       set -eu
       set -f # disable globbing
       export IFS=' '
-
-      echo "Uploading paths" $OUT_PATHS
-      exec nix copy --to "s3://nix-cache?region=mini-nix&compression=xz&endpoint=minio-api.eyen.ca&profile=hercules&parallel-compression=true&secret-key=${nix-server-secret}" $OUT_PATHS
+      if [[ "$1" == "" ]]; then
+        echo "Missing path to build file"
+        exit 1
+      fi
+      nix build -f "$1" --json  --no-link | ${pkgs.jq}/bin/jq '.[].outputs.out' | xargs nix copy --to "s3://nix-cache?region=mini-nix&want-mass-query=true&compression=xz&endpoint=minio-api.eyen.ca&profile=hercules&parallel-compression=true&secret-key=${nix-server-secret}"
     '';
 in {
+  environment.systemPackages = [upload-cache-script];
   sops = {
     # This is the actual specification of the secrets.
     secrets."nix-serve.private" = {
@@ -28,9 +31,5 @@ in {
       path = "/root/.aws/credentials";
       sopsFile = ../secrets/hercules.yaml;
     };
-  };
-
-  nix.settings = {
-    post-build-hook = [upload-cache-script];
   };
 }
