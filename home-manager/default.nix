d98f92ab2d846614da95f7ea31a372682ecdef0b
Eric Yen <eric@ericyen.com>
2023-09-23 18:34:27 -0700

    migrate from byobu to tmux

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -87,7 +87,7 @@ in {
     # viber
     btop
     dua
-    byobu
+    #byobu
     tmux
     ##
     thefuck
@@ -116,7 +116,7 @@ in {
           "docker"
           "docker-compose"
           "celery" # adds completion for python celery
-          "zoxide" # simple naviation with z and history
+          "zoxide" # simple navigation with z and history
           "tmux" # adds aliases to tmux
           "extract" # creates a command extract and alias x to quickly extract files
           "dircycle" # doesn't seem to work on mini-nix -- let's me use `ctrl + shift + <left/right>` to cycle through my cd paths like a browser would
@@ -128,6 +128,17 @@ in {
         ];
         theme = "robbyrussell";
       };
+    };
+
+    tmux = {
+      enable = true;
+      terminal = "screen-256color";
+      shortcut = "a";
+      shell = "${pkgs.zsh}/bin/zsh";
+      sensibleOnTop = true;
+      baseIndex = 1;
+
+      tmuxinator.enable = true;
     };
 
     bat.enable = true; # a cat like tool, but with wings?
@@ -377,13 +388,9 @@ in {
         type = "lua";
       }
       {
-        plugin = vim-LanguageTool;
-        config = ''
-          let g:languagetool_jar = '${pkgs.languagetool}/share/languagetool-commandline.jar'
-          let g:languagetool_cmd = '${pkgs.languagetool}/bin/languagetool-commandline'
-        '';
+        # spelunker is a spell checker
+        plugin = spelunker-vim;
       }
-      {plugin = spelunker-vim;}
       {
         plugin = efmls-configs-nvim;
         type = "lua";
@@ -395,7 +402,7 @@ in {
           local alejandra = require('efmls-configs.formatters.alejandra')
           local cspell = require('efmls-configs.linters.cspell')
           local languages = {
-            ['='] = {cspell,},
+            -- ['='] = {cspell,},
             python = { black,  },
             cpp = { clang_format,  },
             nix = { alejandra,  },
@@ -465,62 +472,62 @@ in {
         plugin = nvim-lspconfig;
         type = "lua";
         config = ''
-          -- Diagnostic keymaps
-          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-          vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-          vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+            -- Diagnostic keymaps
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+            vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+            vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
-          local lspconfig = require('lspconfig')
-          local capabilities = require("cmp_nvim_lsp").default_capabilities()
-          lspconfig.pyright.setup {capabilities = capabilities}
-          lspconfig.nil_ls.setup {capabilities = capabilities}
-          lspconfig.clangd.setup {capabilities = capabilities}
-          lspconfig.cmake.setup {capabilities = capabilities}
-          lspconfig.ruff_lsp.setup {
-              cmd = { "${pkgs.ruff-lsp}/bin/ruff-lsp" },
-              capabilities = capabilities
-          }
+            local lspconfig = require('lspconfig')
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            lspconfig.pyright.setup {capabilities = capabilities}
+            lspconfig.nil_ls.setup {capabilities = capabilities}
+            lspconfig.clangd.setup {capabilities = capabilities}
+            lspconfig.cmake.setup {capabilities = capabilities}
+            lspconfig.ruff_lsp.setup {
+                cmd = { "${pkgs.ruff-lsp}/bin/ruff-lsp" },
+                capabilities = capabilities
+            }
 
 
-          -- Global mappings.
-          -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-          vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-          vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-          vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+            -- Global mappings.
+            -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+            vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
-          -- Use LspAttach autocommand to only map the following keys
-          -- after the language server attaches to the current buffer
-          vim.api.nvim_create_autocmd('LspAttach', {
+            -- Use LspAttach autocommand to only map the following keys
+            -- after the language server attaches to the current buffer
+            vim.api.nvim_create_autocmd('LspAttach', {
 
-          group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-          callback = function(ev)
-          -- Enable completion triggered by <c-x><c-o>
-          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+            -- Enable completion triggered by <c-x><c-o>
+            vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-          -- Buffer local mappings.
-          -- See `:help vim.lsp.*` for documentation on any of the below functions
-          local opts = { buffer = ev.buf }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-          vim.keymap.set('n', '<space>wl', function()
-                vim.print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            -- Buffer local mappings.
+            -- See `:help vim.lsp.*` for documentation on any of the below functions
+            local opts = { buffer = ev.buf }
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+            vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+            vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+            vim.keymap.set('n', '<space>wl', function()
+                  vim.print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+              end, opts)
+            vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+            vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+            vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
             end, opts)
-          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          vim.keymap.set('n', '<space>f', function()
-          vim.lsp.buf.format { async = true }
-          end, opts)
-          end,
-        })
+            end,
+          })
 
         '';
       }
