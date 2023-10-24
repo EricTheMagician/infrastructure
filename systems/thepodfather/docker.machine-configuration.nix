169fc4d96111b27747e0219d6c46dc71649771ec
Eric Yen <eric@ericyen.com>
2023-10-24 00:01:19 -0700

    additional nextcloud configuration for whisper

--- systems/thepodfather/docker.machine-configuration.nix
+++ systems/thepodfather/docker.machine-configuration.nix
@@ -16,6 +16,7 @@ in {
     ./keycloak.nix
     ./lldap.nix
     ./nextcloud.nix
+    #./vaultwarden.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
@@ -83,17 +84,20 @@ in {
   # networking.firewall.enable = false;
 
   environment.systemPackages = [
-    pkgs.unstable.arion
-
-    # Do install the docker CLI to talk to podman.
-    # Not needed when virtualisation.docker.enable = true;
-    pkgs.unstable.docker-client
+    #pkgs.unstable.docker-client
   ];
   virtualisation.docker.enable = false;
   virtualisation.podman = {
     enable = true;
+    package = pkgs.unstable.podman;
     dockerSocket.enable = true;
+    # Create a `docker` alias for podman, to use it as a drop-in replacement
+    dockerCompat = true;
+
+    # Required for containers under podman-compose to be able to talk to each other.
+    defaultNetwork.settings.dns_enabled = false;
   };
+  #virtualisation.oci-containers.backend = "docker"; # this is only needed for collabora online server
 
   hardware.opengl = {
     enable = true;
@@ -113,6 +117,7 @@ in {
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
+  programs.nix-ld.enable = true; # needed for whisper
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -120,4 +125,5 @@ in {
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
+  #services.qemuGuest.enable = true;
 }
