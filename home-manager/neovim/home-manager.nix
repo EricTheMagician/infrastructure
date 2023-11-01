66c8c6778d983735ca4f270edb2744ce03ed5506
Eric Yen <eric@ericyen.com>
2023-11-01 06:48:01 -0700

    disable auto-format on save

--- home-manager/neovim/home-manager.nix
+++ home-manager/neovim/home-manager.nix
@@ -265,19 +265,20 @@ in {
           capabilities = require("cmp_nvim_lsp").default_capabilities(),
         }))
         local lsp_fmt_group = vim.api.nvim_create_augroup('LspFormattingGroup', {})
-        vim.api.nvim_create_autocmd('BufWritePost', {
-          group = lsp_fmt_group,
-          callback = function()
-            local efm = vim.lsp.get_active_clients({ name = 'efm' })
-
-            if vim.tbl_isempty(efm) then
-              return
-            end
-
-          vim.lsp.buf.format({ name = 'efm' })
-            end,
-        })
       '';
+      # code for auto format on save for vim code
+      #vim.api.nvim_create_autocmd('BufWritePost', {
+      #  group = lsp_fmt_group,
+      #  callback = function()
+      #    local efm = vim.lsp.get_active_clients({ name = 'efm' })
+
+      #    if vim.tbl_isempty(efm) then
+      #      return
+      #    end
+
+      #    vim.lsp.buf.format({ name = 'efm' })
+      #    end,
+      #  })
     }
     {
       plugin = neorg;
