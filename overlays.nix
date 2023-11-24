12f906311058ebd471f04812936cf9a2a8ec2f64
Eric Yen <eric@ericyen.com>
2023-11-24 09:22:47 -0800

    add rust patch until it lands in unstable (for lldap)

--- overlays.nix
+++ overlays.nix
@@ -28,8 +28,24 @@
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
-  unstable-packages = final: _prev: {
-    unstable = import inputs.nixpkgs-unstable {
+  unstable-packages = final: _prev: let
+    inherit (final) system;
+
+    patches = [
+      {
+        # patch for lldap: https://github.com/NixOS/nixpkgs/pull/268168
+        url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/268168.patch";
+        hash = "sha256-WIMDnmZV0eL1eFVD0ldHUBrulZWsjdFOmcN4i8+RgFA=";
+      }
+    ];
+    originPkgs = inputs.nixpkgs-unstable.legacyPackages.${system};
+    nixpkgs-unstable = originPkgs.applyPatches {
+      name = "nixpkgs-patched";
+      src = inputs.nixpkgs;
+      patches = map originPkgs.fetchpatch patches;
+    };
+  in {
+    unstable = import nixpkgs-unstable {
       inherit (final) system;
       config.allowUnfree = true;
       overlays = [my_vim_plugins];
