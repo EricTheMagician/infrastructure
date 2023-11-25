908ccae9d4b23b7af9c6c1996fdf621159b2548e
Eric Yen <eric@ericyen.com>
2023-11-24 19:25:16 -0800

    ensure immich is only loaded after certain dependencies have started

--- systems/thepodfather/docker-compose/immich.nix
+++ systems/thepodfather/docker-compose/immich.nix
@@ -51,6 +51,10 @@ in
       enable = true;
     };
 
+    systemd.services.arion-immich = {
+      after = ["redis-immich.service" "typesense.service" "postgresql.service"];
+    };
+
     services.typesense = {
       enable = true;
       apiKeyFile = config.sops.secrets."immich/typesense_api".path;
@@ -97,38 +101,6 @@ in
         env_file = [env_file];
         restart = "unless-stopped";
       };
-      #typesense = {
-      #  container_name = "immich_typesense";
-      #  image = "typesense/typesense:0.24.1@sha256:9bcff2b829f12074426ca044b56160ca9d777a0c488303469143dd9f8259d4dd";
-      #  env_file = [typesense_env];
-      #  #environment =
-      #  #  - TYPESENSE_API_KEY=${TYPESENSE_API_KEY}
-      #  #  - TYPESENSE_DATA_DIR=/data
-      #  #  # remove this to get debug messages
-      #  #  - GLOG_minloglevel=1
-      #  volumes = [
-      #    "tsdata:/data"
-      #  ];
-      #  restart = "unless-stopped";
-      #};
-      # redis ={
-      #   container_name = "immich_redis";
-      #   image = "redis:6.2-alpine@sha256:70a7a5b641117670beae0d80658430853896b5ef269ccf00d1827427e3263fa3";
-      #   restart = "unless-stopped";
-      #   };
-
-      # database =
-      #   container_name = immich_postgres
-      #   image = postgres:14-alpine@sha256:28407a9961e76f2d285dc6991e8e48893503cc3836a4755bbc2d40bcc272a441
-      #   env_file =
-      #     - .env
-      #   environment =
-      #     POSTGRES_PASSWORD = ${DB_PASSWORD}
-      #     POSTGRES_USER = ${DB_USERNAME}
-      #     POSTGRES_DB = ${DB_DATABASE_NAME}
-      #   volumes =
-      #     - pgdata:/var/lib/postgresql/data
-      #   restart = unless-stopped
     };
 
     services.nginx.virtualHosts."immich.eyen.ca" = {
