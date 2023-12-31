d3c43ffcfb0d7435b1568858c865e771c7e7d5e4
Eric Yen <eric@ericyen.com>
2023-12-30 21:21:02 -0800

    add nginx hardening from the wiki

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -81,6 +81,30 @@ in {
               IQ no;
               KP no;
             }
+            # additional settins from the nixos.wiki/wiki/nginx about hardening
+
+            # Add HSTS header with preloading to HTTPS requests.
+            # Adding this header to HTTP requests is discouraged
+            map $scheme $hsts_header {
+                https   "max-age=31536000; includeSubdomains; preload";
+            }
+            add_header Strict-Transport-Security $hsts_header;
+
+            # Enable CSP for your services.
+            #add_header Content-Security-Policy "script-src 'self'; object-src 'none'; base-uri 'none';" always;
+
+            # Minimize information leaked to other domains
+            add_header 'Referrer-Policy' 'origin-when-cross-origin';
+
+            # Disable embedding as a frame
+            add_header X-Frame-Options DENY;
+
+            # Prevent injection of code in other mime types (XSS Attacks)
+            add_header X-Content-Type-Options nosniff;
+
+            # This might create errors
+            # I commented it out in case it did create an error
+            # proxy_cookie_path / "/; secure; HttpOnly; SameSite=strict";
           '';
         }
       )
