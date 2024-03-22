8a34cd55f938a2623696a2841b07c5ed4321787d
Eric Yen <eric@ericyen.com>
2024-03-21 17:39:49 -0700

    add stepskip command to skip my known files

--- modules/home-manager/gdbinit.nix
+++ modules/home-manager/gdbinit.nix
@@ -38,6 +38,30 @@ in {
         sys.path.insert(0, '${lib.getLib pkgs.gcc-unwrapped}/share/gcc-${pkgs.gcc-unwrapped.version}/python')
         from libstdcxx.v6.printers import register_libstdcxx_printers
         register_libstdcxx_printers (None)
+
+        #######################
+        ## add stepskip
+        #######################
+        import gdb
+
+        class StepSkipping(gdb.Command):
+            """Step into or over based on the source file path"""
+
+            def __init__(self):
+                super(StepSkipping, self).__init__("stepskip", gdb.COMMAND_USER)
+
+            def invoke(self, arg, from_tty):
+                while True:
+                    gdb.execute("step")
+                    frame = gdb.selected_frame()
+                    sal = frame.find_sal()
+                    fname = sal.symtab.fullname() if sal.symtab else ""
+
+                    if not fname.startswith('/opt/rh') and not fname.startswith('/home/conda') and not fname.startswith('/work/conan/.conan/data'):
+                        break
+                    gdb.execute("next")
+        StepSkipping()
+
         end
       ''
       + (lib.optionalString cfg.pretty-print.qt.enable ''
