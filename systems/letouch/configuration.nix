83754c1b7d9642a85e7082095b46a2bf15feea1d
Eric Yen <eric@ericyen.com>
2024-03-23 13:58:54 -0700

    disable vscode on native letouch. use only the one in home-manager

--- systems/letouch/configuration.nix
+++ systems/letouch/configuration.nix
@@ -123,38 +123,44 @@
     unstable.chromium
     #unstable.ungoogled-chromium
     unstable.nextcloud-client
-    # vscode
-    (
-      unstable.vscode-with-extensions.override
-      {
-        vscodeExtensions = with pkgs.unstable.vscode-extensions;
-          [
-            # generatl development related packages
-            ms-vscode-remote.remote-ssh
-            eamodio.gitlens
-            # ms-vscode.powershell
-            # ms-azuretools.vscode-docker
+    ## vscode
+    #(
+    #  unstable.vscode-with-extensions.override
+    #  {
+    #    vscodeExtensions = with pkgs.unstable.vscode-extensions;
+    #      [
+    #        # generatl development related packages
+    #        ms-vscode-remote.remote-ssh
+    #        eamodio.gitlens
+    #        # ms-vscode.powershell
+    #        # ms-azuretools.vscode-docker
 
-            # python related packages
-            ms-python.python
-            ms-python.vscode-pylance
-            ms-python.black-formatter
+    #        # python related packages
+    #        ms-python.python
+    #        ms-python.vscode-pylance
+    #        ms-python.black-formatter
 
-            # C++ related packages
-            #ms-vscode.cpptools
-            #ms-vscode.cmake-tools
-            #xaver.clang-format
+    #        # C++ related packages
+    #        #ms-vscode.cpptools
+    #        #ms-vscode.cmake-tools
+    #        #xaver.clang-format
 
-            # nix related packages
-            bbenoist.nix
-            arrterian.nix-env-selector
-            jnoortheen.nix-ide
-          ]
-          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace
-          [
-          ];
-      }
-    )
+    #        # nix related packages
+    #        bbenoist.nix
+    #        arrterian.nix-env-selector
+    #        jnoortheen.nix-ide
+    #      ]
+    #      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace
+    #      [
+    #        {
+    #          name = "codeium";
+    #          publisher = "Codeium";
+    #          version = "1.7.34";
+    #          sha256 = "sha256-Wcy3XGlZM1HNaLmFeV1jAn+r6si9vJnUv3DK7HnrisA=";
+    #        }
+    #      ];
+    #  }
+    #)
   ];
   nix = {
     # This will add each flake input as a registry
