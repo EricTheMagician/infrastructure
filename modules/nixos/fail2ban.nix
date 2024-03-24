ca6f709f1bd9b406c98e419d25fdd797b77031c5
Eric Yen <eric@ericyen.com>
2024-03-24 08:06:24 -0700

    add my headscale ip range to the allow list in fail2ban

--- modules/nixos/fail2ban.nix
+++ modules/nixos/fail2ban.nix
@@ -14,6 +14,9 @@ in {
 
   config = mkIf cfg.enable {
     services.fail2ban.enable = true;
+    services.fail2ban.ignoreIP = [
+      "100.64.0.0/10"
+    ];
     services.fail2ban.bantime-increment = {
       enable = true;
       maxtime = "168h"; # 7 days
