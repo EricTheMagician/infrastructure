684e88e34f0e198d978ed059d62c679699c5339f
Eric Yen <eric@ericyen.com>
2024-03-26 02:05:47 -0700

    fix issue incus

--- modules/nixos/incus.nix
+++ modules/nixos/incus.nix
@@ -11,25 +11,20 @@ in {
   options.my.incus = {
     enable = mkEnableOption "incus";
   };
+  disabledModules = ["virtualisation/incus.nix"];
+  imports = [(inputs.nixpkgs-unstable + "/nixos/modules/virtualisation/incus.nix")];
   config = mkIf cfg.enable {
-    disabledModules = ["virtualisation/incus.nix"];
-    imports = [(inputs.nixpkgs-unstable + "/nixos/modules/virtualisation/incus.nix")];
-    options.my.lxd = {
-      enable = mkEnableOption "lxd";
-    };
-    config = mkIf cfg.enable {
-      virtualisation.incus = {
+    virtualisation.incus = {
+      enable = true;
+      package = pkgs.unstable.incus;
+      ui = {
         enable = true;
-        package = pkgs.unstable.incus;
-        ui = {
-          enable = true;
-          package = pkgs.unstable.incus.ui;
-        };
+        package = pkgs.unstable.incus.ui;
       };
-      virtualisation.lxc = {
-        lxcfs.enable = true;
-      };
-      networking.nftables.enable = true;
     };
+    virtualisation.lxc = {
+      lxcfs.enable = true;
+    };
+    networking.nftables.enable = true;
   };
 }
